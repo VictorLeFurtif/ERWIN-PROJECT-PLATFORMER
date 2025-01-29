e6b011aa6076476f221a1182ea14edf7bd762e09
Victor <victor.baz92@gmail.com>
2025-01-29 14:32:43 +0100

    meca de ou zinzin

---
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float x;
+
+    [SerializeField]
+    private Rigidbody2D rigidbody2dPlayer;
+
+    private LayerMask groundLayer = 3;
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private SpriteRenderer spriteRendererPlayer;
+    [SerializeField] private CapsuleCollider2D capsuleCollider2D;
+    [SerializeField] private float valueToAjustJump;
+    [Header("JUMP")]
+    [SerializeField]
+    private  float jumpStrenght;
+    private bool canJump = false;
+    [SerializeField] private float timeToChangeGravity;
+    [SerializeField] private GameObject prefabs;
+    [SerializeField] private float elapsedTime;
+    [SerializeField] private float speedTimeScale;
+    
+    
+    private void Update()
+    {
+        PlayerMoove();
+        //InverseGravity();
+        RaycastToFlags();
+    }
+    
+
+    private void PlayerMoove()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+    
+        rigidbody2dPlayer.velocity = new Vector2(x * moveSpeed, rigidbody2dPlayer.velocity.y);
+        
+        switch (rigidbody2dPlayer.velocity.x)
+        {
+            case > 0:
+                spriteRendererPlayer.flipX = false;
+                break;
+            case < 0:
+                spriteRendererPlayer.flipX = true;
+                break;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            rigidbody2dPlayer.AddForce(new Vector2(0,jumpStrenght), ForceMode2D.Impulse);
+            canJump = false;
+        }
+    }
+
+    private void InverseGravity()
+    {
+        if (Input.GetKeyDown(KeyCode.R) && canJump)
+        {
+            Physics2D.gravity = -Physics2D.gravity;
+            rigidbody2dPlayer.velocity = new Vector2(rigidbody2dPlayer.velocity.x, -rigidbody2dPlayer.velocity.y);
+        }
+    }
+    
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        foreach (var contact in other.contacts)
+        {
+            canJump = contact.normal.y > 0.5f;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        canJump = false;
+    }
+
+    private void TimeS()
+    {
+        elapsedTime += Time.deltaTime;
+    }
+    
+    private void RaycastToFlags()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            elapsedTime = 0;
+        }
+        TimeS();
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (Camera.main == null) return;
+            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log("Clic position : " + cursorPos);
+            GameObject newObj = Instantiate(prefabs,cursorPos,Quaternion.identity);
+            newObj.transform.localScale = new Vector2(elapsedTime*speedTimeScale,newObj.transform.localScale.y);
+        }
+        
+
+        
+    }
+
+}
